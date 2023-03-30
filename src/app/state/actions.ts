9de55d21cf32a9ec62bd44b4d3bb6db62acfa46a
Milssy000 <113454078+Milssy000@users.noreply.github.com>
2023-03-30 10:04:13 +0200

    Update actions.ts

---
+++ src/app/state/actions.ts
@@ -0,0 +1,19 @@
+// import { Product } from "../products/product.interface";
+// import {services} from '../services';
+// import {products} from '../';
+
+
+
+// export class GetOrders {
+//     static readonly type = '[Orders] Get Orders';
+// }
+
+// export class SetFilter {
+//     static readonly type = '[Orders] Set Filter';
+//     constructor(public orderFilter: Product) { }
+// }
+
+// export class product$ {
+//     static readonly type = '[Orders] Scroll Results';
+//     constructor(public direction: Product) { }
+// }
