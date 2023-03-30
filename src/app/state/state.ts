db77b6380865b440483d31431f378fd9bcb2efe7
Milssy000 <113454078+Milssy000@users.noreply.github.com>
2023-03-30 10:05:01 +0200

    Update state.ts

---
+++ src/app/state/state.ts
@@ -0,0 +1,32 @@
+// import { tap } from 'rxjs/operators';
+// import { Action, Selector, State, StateContext } from '@ngxs/store';
+// import{products}  from '../products';
+// import {FavouriteService, index} from '../services/index';
+// import { Injectable } from '@angular/core';
+// import { GetOrders } from './actions';
+
+
+
+
+
+
+   
+// @Injectable()
+// export class product$ {
+
+//     @Selector()
+//     static currentPage(state: products): number {
+//         return state.orderFilter.page;
+//     }
+
+//     @Selector()
+//     static itemsOnPage(state: products): number {
+//         return state.orderFilter.itemsOnPage;
+//     }
+
+   
+//         }
+        
+//     constructor() { }
+
+   
